4cdd0244d23d1fe7d21543a0df150cd5761ef869
Tim Docker <tim@dockerz.net>
2022-02-26 16:07:34 +1100

    Include src code

--- tools/esm-publish.sh
+++ tools/esm-publish.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
 
-ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )
+REPO=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )
 echo "** Building...."
-(cd $ROOT_DIR; yarn build)
+(cd $REPO; yarn build)
 
-# Tidy the package file for publishing
-jq 'del(.scripts)|del(.devDependencies)|del(.files)' <$ROOT_DIR/package.json  >$ROOT_DIR/dist/package.json
+# Update the dist dir reaady for publishing
+cp -r $REPO/src $REPO/dist
+jq 'del(.scripts)|del(.devDependencies)|del(.files)' <$REPO/package.json  >$REPO/dist/package.json
 
 echo "** Publishing...."
-(cd $ROOT_DIR/dist; npm publish)
+(cd $REPO/dist; npm publish)
